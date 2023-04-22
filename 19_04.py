573f671be9aef68ad2ab3212b11a6853bd54af27
moky1477 <oswalmokshit09@gmail.com>
2023-04-22 15:57:55 +0530

    22_04

--- 19_04.py
+++ 19_04.py
@@ -6,10 +6,10 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model("C:/Users/Asus/Downloads/Model - 12-04/converted_keras (5)/keras_model.h5", compile=False)
+model = load_model("C:/Users/Asus/Downloads/Model - 19-04/converted_keras (5)/keras_model.h5", compile=False)
 
 # Load the labels
-class_names = open("C:/Users/Asus/Downloads/Model - 12-04/converted_keras (5)/labels.txt", "r").readlines()
+class_names = open("C:/Users/Asus/Downloads/Model - 19-04/converted_keras (5)/labels.txt", "r").readlines()
 
 # Create the array of the right shape to feed into the keras model
 # The 'length' or number of images you can put into the array is
@@ -17,7 +17,7 @@ class_names = open("C:/Users/Asus/Downloads/Model - 12-04/converted_keras (5)/la
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open("C:/Users/Asus/Downloads/cd479eee-2160-47b5-a515-d1596aedc75a.jfif").convert("RGB")
+image = Image.open("C:/Users/Asus/Desktop/Final-Dataset-1/val/Organic/O_10903.jpg").convert("RGB")
 
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
